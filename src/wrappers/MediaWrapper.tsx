406ef55b182fb14537abf8f98f3ee1410f5bebe4
Maxime Tournier <speedrun.fra@gmail.com>
2026-01-16 22:04:48 -0500

    WIP

--- src/wrappers/MediaWrapper.tsx
+++ src/wrappers/MediaWrapper.tsx
@@ -3,14 +3,20 @@ import Downloads from "../feature/media/components/Downloads"
 import Modal from "../feature/media/components/Modal"
 import type { PropsFilmSerie } from "../feature/media/type/media"
 
-export default function MediaWrapper({ props }: { props: PropsFilmSerie }) {
+export default function MediaWrapper({
+  data,
+  type,
+}: {
+  data: PropsFilmSerie
+  type: PropsFilmSerie["type"]
+}) {
   return (
     <MediaProvider>
       <div className="flex md:flex-row md:justify-between flex-col w-full">
         <Downloads
-          downloads={props.downloads}
-          type={props.type}
-          downloadsEpisode={props.downloadsEpisode}
+          downloads={data.downloads}
+          type={type}
+          downloadsEpisode={data.downloadsEpisode}
         />
         <Modal />
       </div>
