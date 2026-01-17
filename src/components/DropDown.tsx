d622f7787e305ad798970e57a35d1a9f51c91ca6
Maxime Tournier <speedrun.fra@gmail.com>
2026-01-17 18:10:43 -0500

    fix css

--- src/components/DropDown.tsx
+++ src/components/DropDown.tsx
@@ -18,7 +18,7 @@ export default function DropDown({ title, label, children }: Dropdown) {
     <div className="flex flex-col md:w-5/12 mb-4">
       <h2 className="text-lg font-semibold mb-2">{title}</h2>
       <button
-        className="w-full text-left bg-gray-300 px-4 py-2 rounded-md flex justify-between items-center cursor-pointer"
+        className="w-full text-left bg-white border border-slate-400 px-4 py-2 rounded-md flex justify-between items-center cursor-pointer"
         onClick={handleOpen}
       >
         {label}
--- src/wrappers/MediaWrapper.tsx
+++ src/wrappers/MediaWrapper.tsx
@@ -1,13 +1,13 @@
 import { MediaProvider } from "../feature/media/context/MediaProvider"
 import Downloads from "../feature/media/components/Downloads"
 import Modal from "../feature/media/components/Modal"
-import type { PropsFilmSerie } from "../feature/media/type/media"
+import type { Media, PropsFilmSerie } from "../feature/media/type/media"
 
 export default function MediaWrapper({
   data,
   type,
 }: {
-  data: PropsFilmSerie
+  data: Media
   type: PropsFilmSerie["type"]
 }) {
   return (
