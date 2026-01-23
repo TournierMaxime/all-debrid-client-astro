e185ce07216d1948ac1156fb5532eb768e411649
Maxime Tournier <speedrun.fra@gmail.com>
2026-01-22 21:19:07 -0500

    fix css responsive

--- src/feature/search/components/Search.tsx
+++ src/feature/search/components/Search.tsx
@@ -27,7 +27,7 @@ const SearchForm = () => {
   return (
     <div className="flex flex-col justify-center items-center md:flex-row md:justify-center w-2/12">
       <div className="flex items-center" onClick={handleOpen}>
-        <FaSearch className="sm:w-[50px] sm:h-[50px] w-[40px] h-[40px] max-w-[50px] cursor-pointer mr-4" />
+        <FaSearch className="md:w-[40px] md:h-[40px] w-[30px] h-[30px] max-w-[40px] cursor-pointer mr-4" />
       </div>
       <Modal isOpen={isOpen} onClose={handleOpen} title="Moteur de recherche">
         <Form handleSubmit={handleSubmit} />
