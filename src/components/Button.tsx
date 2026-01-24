0b9151f4a1ca3eb992896b1ef789329c729a8218
Maxime Tournier <speedrun.fra@gmail.com>
2026-01-24 13:55:33 -0500

    fix css

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,28 @@
-import React from "react"
-
 type Props = {
-  children: React.ReactNode
-  onClick: () => void
+  children?: React.ReactNode
+  onClick?: any
+  title?: string
+  clx?: string
 }
 
-const Button = ({ children, onClick }: Props) => {
+const Button = ({ children, onClick, title, clx }: Props) => {
   return (
     <button
-      className="mt-4 px-4 py-2 bg-blue-500 text-white rounded cursor-pointer"
+      className={`m-1 px-2 py-1 bg-blue-500 text-white rounded cursor-pointer text-sm ${clx}`}
       onClick={onClick}
+      title={title}
+    >
+      {children}
+    </button>
+  )
+}
+
+export const DeleteButton = ({ children, onClick, title, clx }: Props) => {
+  return (
+    <button
+      className={`m-1 px-2 py-1 bg-red-500 text-white rounded cursor-pointer text-sm ${clx}`}
+      onClick={onClick}
+      title={title}
     >
       {children}
     </button>
