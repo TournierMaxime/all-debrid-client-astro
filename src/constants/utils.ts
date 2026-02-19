8c23f223f68c9659f68c080d7ba5d489b7559646
Maxime Tournier <speedrun.fra@gmail.com>
2026-02-19 17:05:36 -0500

    refacto lib

--- src/constants/utils.ts
+++ src/constants/utils.ts
@@ -6,21 +6,3 @@ export const zaFullUrl = "https://www.wawacity.irish"
 
 export const plexEndpoint = SECRET_PLEX_ENDPOINT
 export const plexToken = SECRET_PLEX_TOKEN
-
-export const formatDuration = (durationMs: number): string => {
-  const totalMinutes = Math.floor(durationMs / 1000 / 60)
-  const hours = Math.floor(totalMinutes / 60)
-  const minutes = totalMinutes % 60
-
-  return `${hours}h ${minutes}min`
-}
-
-export const formatBytes = (bytes: number) => {
-  if (bytes === 0) return "0 B"
-
-  const k = 1024
-  const sizes = ["B", "KB", "MB", "GB", "TB"]
-  const i = Math.floor(Math.log(bytes) / Math.log(k))
-
-  return `${(bytes / Math.pow(k, i)).toFixed(2)} ${sizes[i]}`
-}
