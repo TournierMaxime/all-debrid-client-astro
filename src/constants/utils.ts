6cd0cd9f7615ae9720853cdccc728b01589784f0
Maxime Tournier <speedrun.fra@gmail.com>
2026-02-24 15:42:20 -0500

    fix domain & docker dev conf

--- src/constants/utils.ts
+++ src/constants/utils.ts
@@ -1,8 +1,8 @@
-import { SECRET_PLEX_ENDPOINT,SECRET_PLEX_TOKEN } from "astro:env/server"
+import { SECRET_PLEX_ENDPOINT, SECRET_PLEX_TOKEN } from "astro:env/server"
 
-export const za = "www.wawacity.irish"
+export const za = "www.wawacity.rent"
 export const maxWidth = "max-w-[1024px]"
-export const zaFullUrl = "https://www.wawacity.irish"
+export const zaFullUrl = "https://www.wawacity.rent"
 
 export const plexEndpoint = SECRET_PLEX_ENDPOINT
 export const plexToken = SECRET_PLEX_TOKEN
