9e5274ee1d1a2a892ada12b1db62f10a72a54cc1
Maxime Tournier <pronto-dairy-wafer@duck.com>
2026-03-17 18:00:24 -0400

    move to shared

---
+++ src/components/shared/Alert.tsx
@@ -0,0 +1,108 @@
+import {
+  Alert as AlertShadcn,
+  AlertDescription,
+  AlertTitle,
+} from "@/components/ui/alert"
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog"
+
+type AlertProps = {
+  open?: boolean
+  onCancel?: (param?: unknown) => void
+  onConfirm?: () => void
+  title: string
+  description?: string
+  isChoice?: boolean
+  children?: React.ReactNode
+}
+
+type MessageProps = {
+  message?: string
+  error?: string
+}
+
+export default function Alert({
+  open,
+  onCancel,
+  onConfirm,
+  title,
+  description,
+  isChoice,
+  children,
+}: AlertProps) {
+  if (!open) return null
+  return (
+    <>
+      <div className="fixed inset-0 bg-(--ads-bg-overlay) z-40" />
+      <Dialog
+        modal={false}
+        open={open}
+        onOpenChange={(v) => (onCancel ? !v && onCancel() : !v)}
+      >
+        <DialogContent className="z-50">
+          <DialogHeader>
+            <DialogTitle>{title}</DialogTitle>
+            <DialogDescription>{description}</DialogDescription>
+          </DialogHeader>
+          {isChoice ? (
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button className="cursor-pointer" onClick={onCancel}>
+                  Annuler
+                </Button>
+              </DialogClose>
+              <Button
+                className="bg-(--ads-btn-delete) cursor-pointer mb-4 md:mb-0"
+                onClick={onConfirm}
+              >
+                Confirmer
+              </Button>
+            </DialogFooter>
+          ) : null}
+          {children}
+        </DialogContent>
+      </Dialog>
+    </>
+  )
+}
+
+export function Message({ message, error }: MessageProps) {
+  return error ? (
+    <p className="w-full my-4 text-(--ads-text-error)">{error}</p>
+  ) : (
+    <p className="w-full my-4 text-(--ads-text-success)">{message}</p>
+  )
+}
+
+export function AlertMessage({
+  title,
+  description,
+  backgroundColor = "bg-(--ads-alert-bg-default)",
+  borderColor = "border-(--ads-alert-border-default)",
+  color = "text-(--ads-alert-text-default)",
+  clx,
+}: {
+  title: string
+  description?: string
+  backgroundColor?: string
+  borderColor?: string
+  color?: string
+  clx?: string
+}) {
+  return (
+    <AlertShadcn
+      className={`${backgroundColor} ${borderColor} ${color} p-4 rounded mb-2 ${clx ?? ""}`}
+    >
+      <AlertTitle className="mb-2 text-[18px]">{title}</AlertTitle>
+      <AlertDescription className="text-[16px]">{description}</AlertDescription>
+    </AlertShadcn>
+  )
+}
---
+++ src/components/shared/DropDown.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react"
+import { FaChevronDown, FaChevronUp } from "react-icons/fa"
+
+type Dropdown = {
+  title: string
+  label: string
+  children: React.ReactNode
+}
+
+export default function DropDown({ title, label, children }: Dropdown) {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handleOpen = () => {
+    setIsOpen(!isOpen)
+  }
+
+  return (
+    <div className="flex flex-col md:w-5/12 mb-4">
+      <h2 className="text-lg font-semibold mb-2">{title}</h2>
+      <button
+        className="w-full text-left bg-(--ads-bg-default) border border-(--ads-border-dropdown) px-4 py-2 rounded-md flex justify-between items-center cursor-pointer"
+        onClick={handleOpen}
+      >
+        {label}
+        <span className="ml-2">
+          {isOpen ? <FaChevronUp size={24} /> : <FaChevronDown size={24} />}
+        </span>
+      </button>
+      {isOpen && children}
+    </div>
+  )
+}
